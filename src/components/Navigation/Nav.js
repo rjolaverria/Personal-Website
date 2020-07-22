e5799f672a1c6d66b090b2c6a6de2ba6fc0da505
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-07-22 19:27:25 -0400

    Navigation appearance

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,7 +1,11 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
-const Nav = () => {
+const Nav = (props) => {
+  useEffect(() => {
+    props.history.listen(() => console.log(window.location.pathname));
+  }, [props.history]);
+
   return (
     <Fragment>
       <nav className='side-nav'>
@@ -41,28 +45,31 @@ const Nav = () => {
       </nav>
       <nav className='bottom-nav'>
         <span>&copy; Olaverria Company LLC - 2020</span>
-        <div className='social-bottom'>
-          <a
-            href='https://www.linkedin.com/in/RafiOlaverria'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <i className='fab fa-linkedin-in'></i>
-          </a>
-          <a
-            href='https://github.com/rjolaverria'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <i className='fab fa-github-alt'></i>
-          </a>
-          <a href='#resume'>
-            <i className='fas fa-file'></i>
-          </a>
-        </div>
+        <a
+          className='social-bottom'
+          href='https://www.linkedin.com/in/RafiOlaverria'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <i className='fab fa-linkedin-in'></i>
+        </a>
+        <a
+          className='social-bottom'
+          href='https://github.com/rjolaverria'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <i className='fab fa-github-alt'></i>
+        </a>
+        <a className='social-bottom' href='#resume'>
+          <i className='fas fa-file'></i>
+        </a>
+        <span className='bottom-info'>UTF-8</span>
+        <span className='bottom-info'>React.js</span>
+        <span className='bottom-info'>Prettier: &#10003;</span>
       </nav>
     </Fragment>
   );
 };
 
-export default Nav;
+export default withRouter(Nav);
