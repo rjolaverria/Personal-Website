6d9d40d1a379598fa4e4ddcd3dbbb841eadac342
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-08-05 22:36:29 -0400

    Refactor Navigation Components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 // Components
-import Nav from './components/Navigation/Nav';
+import Navigation from './components/Navigation/Navigation';
 import Home from './components/Main/Home';
 import Projects from './components/Main/Projects';
 import Blog from './components/Main/Blog';
@@ -12,7 +12,7 @@ import About from './components/Main/About';
 const App = () => (
   <Router>
     <Fragment>
-      <Nav />
+      <Navigation />
       <main>
         <Switch>
           <Route exact path='/'>
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Navigation = () => (
+  <Fragment>
+    <Navbar />
+    <Footer />
+  </Fragment>
+);
+
+export default Navigation;
