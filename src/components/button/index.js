fc47ee71ca56d566167d75b960e2b7a378e34ca8
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-08-30 23:38:41 -0400

    Fixed bug

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from './styles';
 
-const Button = ({ children, to, href, ...restProps }) => (
-    <Container {...restProps}>
-        {to ? (
-            <Link to={to}>{children}</Link>
-        ) : href ? (
-            <a href={href} target='_blank' rel='noopener noreferrer'>
-                {children}
-            </a>
-        ) : (
-            children
-        )}
-    </Container>
-);
+const Button = ({ children, to, href, ...restProps }) =>
+    to ? (
+        <Link to={to}>
+            <Container {...restProps}>{children}</Container>
+        </Link>
+    ) : href ? (
+        <a href={href} target='_blank' rel='noopener noreferrer'>
+            <Container {...restProps}>{children}</Container>
+        </a>
+    ) : (
+        <Container {...restProps}>{children}</Container>
+    );
 
 export default Button;
