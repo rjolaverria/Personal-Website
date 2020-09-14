693e8c24db7c4c2e112c93e239c9c85ed7829747
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-09-13 23:25:15 -0400

    Fixed bugs

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Container } from './styles';
+import { Container, Anchor, Link } from './styles';
 
 const Button = ({ children, to, href, ...restProps }) =>
     to ? (
-        <Link to={to}>
-            <Container {...restProps}>{children}</Container>
+        <Link to={to} {...restProps}>
+            {children}
         </Link>
     ) : href ? (
-        <a href={href} target='_blank' rel='noopener noreferrer'>
-            <Container {...restProps}>{children}</Container>
-        </a>
+        <Anchor
+            href={href}
+            target='_blank'
+            rel='noopener noreferrer'
+            {...restProps}
+        >
+            {children}
+        </Anchor>
     ) : (
         <Container {...restProps}>{children}</Container>
     );
--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,18 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Link as RouterLink } from 'react-router-dom';
 
-export const Container = styled.button`
+const style = css`
     display: inline-block;
-    color: ${({ primary, secondary }) =>
-        primary || secondary ? '#fff' : 'var(--primary-color)'};
-    background-color: ${({ primary, secondary }) =>
-        primary ? '#0066B8' : secondary ? 'var(--secondary-color)' : '#fff'};
-    border: none;
-    ${({ border, primary, secondary }) =>
-        border && primary
-            ? 'border: 3px solid var(--secondary-color)'
-            : border
-            ? 'border: 3px solid var(--primary-color)'
-            : ''};
+    text-decoration: none;
+    vertical-align: middle;
+    text-align: center;
+    align-self: flex-start;
+    user-select: none;
     padding: 0.5rem 0.875rem;
     font-size: 1rem;
     font-weight: 600;
@@ -20,14 +15,31 @@ export const Container = styled.button`
     margin: 4px;
     box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.3);
     transition: 1s all;
+    color: ${({ primary, secondary }) =>
+        primary || secondary ? '#fff' : 'var(--primary-color)'};
+    background-color: ${({ primary, secondary }) =>
+        primary ? '#0066B8' : secondary ? 'var(--secondary-color)' : '#fff'};
+    ${({ border, primary, secondary }) =>
+        border && primary
+            ? 'border: 3px solid var(--secondary-color)'
+            : border
+            ? 'border: 3px solid var(--primary-color)'
+            : 'border: none'};
 
     &:hover {
         ${({ primary, secondary }) =>
             primary || secondary ? 'opacity: 0.8' : 'filter: brightness(90%)'};
     }
+`;
 
-    a {
-        text-decoration: none;
-        color: inherit;
-    }
+export const Container = styled.button`
+    ${style}
+`;
+
+export const Anchor = styled.a`
+    ${style}
+`;
+
+export const Link = styled(RouterLink)`
+    ${style}
 `;
