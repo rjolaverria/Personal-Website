58c18569dfbd36ef3597cc32952aac8a7127cca5
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-09-16 16:52:00 -0400

    Used grid areas

--- src/components/cards/contentCard/styles.js
+++ src/components/cards/contentCard/styles.js
@@ -14,51 +14,30 @@ export const Container = styled.div`
     color: ${({ primary, secondary }) =>
         primary || secondary ? '#fff' : 'var(--secondary-color)'};
     display: grid;
-    grid-template: 1em auto auto 100px 1em / 300px 1em auto auto 1em;
+    grid-gap: 1em;
+    grid-template: auto 1fr 2fr 1fr / minmax(200px, 300px) auto auto;
+    grid-template-areas:
+        'img ... ... ...'
+        'img ttl ttl ...'
+        'img bdy bdy ...'
+        'img inf btn ...'
+        'img ... ... ...';
     box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.2);
 
-    @media (max-width: 725px) {
-        grid-template: 1em auto auto 100px 1em / 200px 1em auto auto 1em;
-    }
-
     @media (max-width: 600px) {
-        grid-template: 200px 1em auto auto 100px 1em / 1em auto auto 1em;
-
-        img {
-            height: 200px;
-            flex-basis: 200px;
-            grid-row: 1;
-            grid-column: 1/ -1;
-        }
-
-        h2 {
-            font-size: 1.75rem;
-            grid-row: 3;
-            grid-column: 2/ -2;
-        }
-
-        p {
-            font-size: 1rem;
-            grid-row: 4;
-            grid-column: 2/ -2;
-        }
-
-        small {
-            grid-row: 5;
-            grid-column: 2;
-        }
-
-        button {
-            grid-row: 5;
-            grid-column: 3;
-        }
+        grid-template: 200px 1fr 3fr 1fr / auto auto;
+        grid-template-areas:
+            'img img img img'
+            '... ttl ttl ...'
+            '... bdy bdy ...'
+            '... inf btn ...'
+            '... ... ... ...';
     }
 `;
 
 export const Image = styled.img`
     object-fit: cover;
-    grid-row: 1/ -1;
-    grid-column: 1;
+    grid-area: img;
     width: 100%;
     height: 100%;
 `;
@@ -67,30 +46,36 @@ export const Title = styled.h2`
     margin: 0;
     font-size: 2.25rem;
     font-weight: 600;
-    grid-row: 2;
-    grid-column: 3/-2;
+    grid-area: ttl;
+
+    @media (max-width: 600px) {
+        font-size: 1.75rem;
+    }
 `;
 
 export const Body = styled.p`
+    margin: 0;
     font-size: 1.1rem;
     line-height: 1.2;
-    grid-row: 3;
-    grid-column: 3/-2;
+    grid-area: bdy;
+
+    @media (max-width: 600px) {
+        font-size: 1rem;
+    }
 `;
 
 export const Info = styled.small`
     font-size: 0.9rem;
     font-weight: 700;
     opacity: 0.8;
-    grid-row: 4;
-    grid-column: 3;
+    grid-area: inf;
     align-self: end;
     justify-self: start;
 `;
 
 export const CardButton = styled(Button)`
-    grid-row: 4;
-    grid-column: 4;
+    margin: 0;
+    grid-area: btn;
     justify-self: end;
     align-self: end;
 `;
