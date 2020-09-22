e499732cd4a2d3df1a2b52cdbb872902c1862d38
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-09-21 23:06:23 -0400

    Book shelf update

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,79 +1,55 @@
 import React, { useContext } from 'react';
-import { Container, Title, Block, BookCard } from '../components';
+import { Container, Title, Book, Loading, Badge } from '../components';
 import { AppContext } from '../context/AppContext';
 
 const Blog = () => {
-    const {
-        books: { currentBook, booksRead },
-    } = useContext(AppContext);
+    const { books } = useContext(AppContext);
+    const { currentBook, favBooks } = books;
 
     return (
         <Container>
             <Title>Blog</Title>
-            <Block.Title>
-                Book Shelf{' | '}
-                <a
-                    href='https://www.goodreads.com/review/list/105003741?shelf=read'
-                    target='_blank'
-                    rel='noreferrer noopener'
-                >
-                    <img
-                        border='0'
-                        alt="Rafi Olaverria's book recommendations, liked quotes, book clubs, book trivia, book lists (read shelf)"
-                        src='https://www.goodreads.com/images/badge/badge1.jpg'
-                    />
-                </a>
-            </Block.Title>
-            <Block.Section>
-                <Block>
-                    <Block.SubTitle>Currently Reading</Block.SubTitle>
-                    {currentBook ? (
-                        <BookCard.Shelf>
-                            <BookCard
-                                big
-                                image={currentBook.image}
-                                link={currentBook.link[0]}
-                            >
-                                <BookCard.Title>
-                                    {currentBook.title}
-                                </BookCard.Title>
-                                <BookCard.Author>
-                                    {currentBook.authors[0].author[0].name}
-                                </BookCard.Author>
-                            </BookCard>
-                        </BookCard.Shelf>
-                    ) : (
-                        'Loading...'
+            {books ? (
+                <Book.Shelf>
+                    <Book.ShelfTitle>
+                        My Book Shelf{' | '}
+                        <a
+                            href='https://www.goodreads.com/review/list/105003741?shelf=read'
+                            target='_blank'
+                            rel='noreferrer noopener'
+                        >
+                            <img
+                                border='0'
+                                alt="Rafi Olaverria's book recommendations, liked quotes, book clubs, book trivia, book lists (read shelf)"
+                                src='https://www.goodreads.com/images/badge/badge1.jpg'
+                            />
+                        </a>
+                    </Book.ShelfTitle>
+                    <Book.ShelfSub>Currently Reading</Book.ShelfSub>
+                    {currentBook && (
+                        <Book
+                            showcase
+                            image={currentBook.image}
+                            link={currentBook.link[0]}
+                            title={currentBook.title}
+                            author={currentBook.authors[0].author[0].name}
+                        />
                     )}
-                </Block>
-                <Block>
-                    <Block.SubTitle>Previously Read</Block.SubTitle>
-                    <BookCard.Shelf>
-                        {booksRead
-                            ? booksRead.map(
-                                  (book, index) =>
-                                      book.isbn[0] > 0 && (
-                                          <BookCard
-                                              key={index}
-                                              image={book.image}
-                                              link={book.link[0]}
-                                          >
-                                              <BookCard.Title>
-                                                  {book.title}
-                                              </BookCard.Title>
-                                              <BookCard.Author>
-                                                  {
-                                                      book.authors[0].author[0]
-                                                          .name
-                                                  }
-                                              </BookCard.Author>
-                                          </BookCard>
-                                      )
-                              )
-                            : 'Loading'}
-                    </BookCard.Shelf>
-                </Block>
-            </Block.Section>
+                    <Book.ShelfSub>Recent favorites</Book.ShelfSub>
+                    {favBooks &&
+                        favBooks.map((book, index) => (
+                            <Book
+                                key={index}
+                                image={book.image}
+                                link={book.link[0]}
+                                title={book.title}
+                                author={book.authors[0].author[0].name}
+                            ></Book>
+                        ))}
+                </Book.Shelf>
+            ) : (
+                <Loading />
+            )}
         </Container>
     );
 };
