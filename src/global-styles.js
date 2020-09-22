1e07741b40865a948f42b79566fcc2d46cd3d03d
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-09-21 22:57:07 -0400

    Added google font to headings

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,25 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-:root {
-    --primary-color: rgb(0, 120, 215);
-    --secondary-color: rgb(43, 43, 43);
-    --light-color: rgb(170, 170, 170);
-}
+    :root {
+        --primary-color: rgb(0, 120, 215);
+        --secondary-color: rgb(43, 43, 43);
+        --light-color: rgb(170, 170, 170);
+    }
 
-body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
-        'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-        'Helvetica Neue', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
+    body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+            'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+            'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
 
-code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-        monospace;
-}
+    code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+            monospace;
+    }
+
+    h2,h3,h4,h5,h6 {
+        font-family: 'Roboto Slab', serif;
+    }
 
 `;
 
