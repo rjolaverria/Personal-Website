b1f459ac7dcdc7617b2f34b0ec4c38b8dae80335
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-10-21 15:23:05 -0400

    margin/padding reset

--- src/global-styles.js
+++ src/global-styles.js
@@ -7,8 +7,12 @@ const GlobalStyles = createGlobalStyle`
         --light-color: rgb(170, 170, 170);
     }
 
-    body {
+    * {
         margin: 0;
+        padding: 0;
+    }
+
+    body {
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
             'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
             'Helvetica Neue', sans-serif;
