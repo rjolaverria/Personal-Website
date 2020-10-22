16c1c6740020f4f0ad876bcdf9d9bcab1b02034e
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-10-22 11:20:27 -0400

    Added filter of repos

--- src/hooks/useRepos.js
+++ src/hooks/useRepos.js
@@ -3,12 +3,20 @@ import { get } from 'axios';
 
 const useRepos = () => {
     const [repos, setRepos] = useState([]);
+    const featured = [
+        'CutHub',
+        'Netflix-Clone', 
+        'Personal-Website',
+        'rockpaperscissors', 
+        'Trackit', 
+        'WeddingBarbers.com'];
 
     useEffect(() => {
         get('https://api.github.com/users/rjolaverria/repos').then((res) =>
-            setRepos(res.data)
+            setRepos(res.data.filter(repo=> featured.includes(repo.name)))
         );
     }, []);
+
     return repos;
 };
 
